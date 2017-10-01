69e0f37c94ef5d633565c46b14f6916e0a820390
Yuqing Zhong <zhong.yuq@husky.neu.edu>
2017-10-01 16:23:37 -0400

    update cheerio part

--- assignment1/crawler.js
+++ assignment1/crawler.js
@@ -21,14 +21,38 @@ console.log('Server is running at' + port);
 
 //download the html
 
+var todo = new Set();
+var visited = [];
+
 var name = seed.split('/');
 name = name[name.length - 1];
 var des = fs.createWriteStream('./downloads/' + name + '.text');
+
+
 request(seed, function (err, response, body) {
         if (err) {
             console.log(err);
         } else if (body) {
             var $ = cheerio.load(body);
+
+            var content = '.mw-parser-output';
+            var urls = $('a',content)
+                .each(function (i, el) {
+
+                    if (!$(this).attr("class:contains('image')")
+                            && $(this).attr('href')) {
+                        var url = $(this).attr('href');
+                        if (url.includes('/wiki')
+                            && !url.includes('/File')
+                            && !url.includes('#')
+                            && !todo.has(url)) {
+                            todo.add(url);
+                        }
+
+                    }
+
+                });
+            console.log(todo);
         }
 
     })
