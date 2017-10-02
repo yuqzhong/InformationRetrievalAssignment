22ae5c0625c74c963c2e54ea3321d2ad944cc9e1
Yuqing Zhong <zhong.yuq@husky.neu.edu>
2017-10-02 00:16:29 -0400

    BEFORE BED Bug not fixed

--- assignment1/crawler.js
+++ assignment1/crawler.js
@@ -13,12 +13,15 @@ console.log('Server is running at ' + port);
 
 var seed = 'https://en.wikipedia.org/wiki/Tropical_cyclone';
 var maxLevel = 6;
-var maxCrawlPageNum = 1000;
-var pauseTime = 1000;
+var maxCrawlPageNum = 300;
+var pauseTime = 50;
 
 
 var todo = [];
+var error = [];
 var visited = new Set();
+var names = new Set();
+
 todo.push([seed]);
 for (var i = 1; i < 6; i++) {
     todo.push([]);
@@ -41,6 +44,7 @@ function crawl() {
 
         request(url, function (err, response, body) {
             if (err) {
+                error.push(url + " " + err);
                 console.log(err);
             } else if (body) {
                 var $ = cheerio.load(body);
@@ -65,35 +69,59 @@ function crawl() {
                         }
 
                     });
+            }
+
+        })
+        .pipe(des);
+
+    des
+        .on('close', function () {
+            visited.add(url);
+            if (names.has(name)) {
+                console.log(url);
+                console.log("!!!!!!!!!!!!!!!!!" + name);
+            } else {
+                names.add(name);
+
                 console.log(todo[level].length);
                 console.log(visited);
                 console.log(visited.size);
                 console.log(level);
+                console.log('File save successfully!');
+
+                if (level < maxLevel && visited.size < maxCrawlPageNum) {
+                    setTimeout(crawl, pauseTime);
+                } else {
+                    outputURLs();
+                }
             }
 
         })
-        .pipe(des)
-        .on('finish', function () {
-            visited.add(url);
-            console.log('File save successfully!');
-            if (level < maxLevel && visited.size < maxCrawlPageNum) {
-                setTimeout(crawl,pauseTime);
-            } else {
-                var outURLs = fs.createWriteStream('./crawledURLs.txt');
-                visited.forEach(function (p1, p2, p3) {
-                   outURLs.write(p1 + " ");
-                });
-                console.log('Finished XD');
-            }
-        })
         .on('error', function (err) {
+            error.push(url + " " + err);
             console.log(err);
         })
+
     } else {
         if (level < maxLevel && visited.size < maxCrawlPageNum) {
             crawl();
+        } else {
+            outputURLs();
         }
     }
+}
+
+function outputURLs() {
+    var outURLs = fs.createWriteStream('./crawledURLs.txt');
+    visited.forEach(function (p1, p2, p3) {
+        outURLs.write(p1 + " ");
+    });
+    var nameOut = fs.createWriteStream('./names.txt');
+    names.forEach(function (p1, p2, p3) {
+        nameOut.write(p1 + "+");
+    });
+    console.log(error);
+    console.log('Finished XD');
 }
 
 crawl();
