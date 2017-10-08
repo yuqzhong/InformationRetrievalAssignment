7b7262dca9e3c9282fce49f819e169ce3f9e0811
Yuqing Zhong <zhong.yuq@husky.neu.edu>
2017-10-08 16:58:30 -0400

    add page change check in both crawler.js and focusedCrawler.js

--- assignment1/crawler.js
+++ assignment1/crawler.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var seed = 'https://en.wikipedia.org/wiki/Tropical_cyclone';
 var maxLevel = 6;
 var maxCrawlPageNum = 1000;
-var pauseTime = 1000;
+var pauseTime = 100;
 
 
 var todo = [];
@@ -30,71 +30,88 @@ function crawl() {
     var lowerURL = url.toLowerCase();
     console.log(url);
     if (!visited.has(lowerURL)) {
-        visited.add(lowerURL);
-        URLs.push(url);
         //download the html
-        var name = url.split('/');
-        name = name[name.length - 1];
-        var des = fs.createWriteStream('./downloads/' + name.toLowerCase() + '.txt');
-
         request(url, function (err, response, body) {
-            console.log(response.statusCode);
+            // console.log(response.statusCode);
             if (err) {
                 console.log(err);
             } else if (response.statusCode === 200 && body) {
                 // console.log(response);
                 var $ = cheerio.load(body);
-                var content = '.mw-parser-output';
+                //check if change to other url
+                var thisUrl = $('link[rel=canonical]').attr('href');
+                if (thisUrl.includes('#')) {
+                    var temp = url.split('#');
+                    thisUrl = temp[0];
+                }
+                console.log(thisUrl);
+                if (!visited.has(thisUrl.toLowerCase())) {
+                    visited.add(thisUrl.toLowerCase());
+                    visited.add(url.toLowerCase());
+                    URLs.push(thisUrl);
+                    var content = '.mw-parser-output';
 
-                $('a',content).each(function (i, el) {
-                    if (!$(this).attr("class:contains('image')")
-                        && $(this).attr('href')) {
-                        var toadd = $(this).attr('href');
-                        if (toadd.includes('/wiki/')
-                            && !toadd.includes(':')
-                            && !toadd.includes('/File')
-                            && !toadd.includes('#')) {
-                            if (toadd.includes('https://en.wikipedia.org')) {
-                                todo[level + 1].push(toadd);
-                            } else {
-                                todo[level + 1].push('https://en.wikipedia.org' + toadd);
+                    $('a',content).each(function (i, el) {
+                        if (!$(this).attr("class:contains('image')")
+                            && $(this).attr('href')) {
+                            var toadd = $(this).attr('href');
+                            if (toadd.includes('/wiki/')
+                                && !toadd.includes(':')
+                                && !toadd.includes('/File')) {
+                                if (toadd.includes('#')) {
+                                    var temp = url.split('#');
+                                    toadd = temp[0];
+                                }
+                                if (toadd.includes('https://en.wikipedia.org')) {
+                                    todo[level + 1].push(toadd);
+                                } else {
+                                    todo[level + 1].push('https://en.wikipedia.org' + toadd);
+                                }
                             }
+
                         }
 
-                    }
+                    });
+                    var name = thisUrl.split('/');
+                    name = name[name.length - 1];
+                    name = name.replace(/[\\*?:"<>|]/g, function (x) {
+                        return x.charCodeAt(0);
+                    });
+                    console.log(name.toLowerCase());
+                    var filename = './downloads/' + name.toLowerCase() + '.txt';
+                    fs.writeFileSync(filename, body);
 
-                });
+
+                    afterFunction();
+                } else {
+                    visited.add(url);
+                    afterFunction();
+                }
             }
 
         })
-        .pipe(des);
-
-        des
-            .on('close', function () {
-                // console.log(todo[level].length);
-                // console.log(visited);
-                // console.log(visited.size);
-                // console.log(level);
-                // console.log('File save successfully!');
-
-                if (level < maxLevel && visited.size < maxCrawlPageNum) {
-                    setTimeout(crawl, pauseTime);
-                } else {
-                    outputURLs();
-                }
-
-            })
-        .on('error', function (err) {
-            error.push(url + " " + err);
-            console.log(err);
-        })
 
     } else {
-        if (level < maxLevel && visited.size < maxCrawlPageNum) {
+        console.log(level);
+        console.log();
+        if (level < maxLevel && URLs.length < maxCrawlPageNum) {
             crawl();
         } else {
             outputURLs();
         }
+    }
+}
+
+function afterFunction() {
+    console.log(todo[level].length);
+    console.log(visited.size);
+    console.log(URLs.length);
+    console.log(level);
+    console.log();
+    if (level < maxLevel && URLs.length < maxCrawlPageNum) {
+        setTimeout(crawl, pauseTime);
+    } else {
+        outputURLs();
     }
 }
 
--- assignment1/focusedCrawler.js
+++ assignment1/focusedCrawler.js
@@ -13,17 +13,100 @@ var pauseTime = 10;
 
 
 var todo = [];
-var ptodo = [];
 var error = [];
 var visited = new Set();
 var URLs = [];
-var counter = 0;
+var level = 0;
 
-var invalidChar = ['\\','/','*', '?', ':', '"', '<', '>', '|'];
 
-todo.push([seed, '', 1]);
-visited.add(seed.toLowerCase());
+todo.push([[seed, '']]);
 // var level = 0;
+
+for (var i = 2; i <= maxLevel; i++) {
+    todo.push([]);
+}
+
+function crawl() {
+    var combo = todo[level].shift();
+    if (combo === undefined) {
+        level++;
+        combo = todo[level].shift();
+    }
+
+    console.log(combo);
+    var url = combo[0];
+    //download the html
+    var name = url.split('/');
+    name = name[name.length - 1];
+    var lowerURL = url.toLowerCase();
+    if (!visited.has(lowerURL)) {
+        // visited.add(lowerURL);
+
+        request(url, function (err, response, body) {
+            if (err) {
+                console.log(err);
+            } else if (response && response.statusCode === 200 && body) {
+                var $ = cheerio.load(body);
+
+                var thisUrl = $('link[rel=canonical]').attr('href');
+                if (thisUrl.includes('#')) {
+                    var temp = url.split('#');
+                    thisUrl = temp[0];
+                }
+                console.log(thisUrl);
+                if (!visited.has(thisUrl.toLowerCase())) {
+                    visited.add(thisUrl.toLowerCase());
+                    visited.add(url.toLowerCase());
+
+                    var content = '.mw-parser-output';
+
+                    $('a', content).each(function (i, el) {
+                        if (!$(this).attr("class:contains('image')")
+                            && $(this).attr('href')) {
+                            var toadd = $(this).attr('href');
+                            var anchor = $(this).attr('title');
+                            if (toadd.includes('/wiki/')
+                                && !toadd.includes(':')
+                                && !toadd.includes('/File')
+                                && !toadd.includes('#')) {
+                                if (level < maxLevel - 1) {
+                                    var urlToAdd = toadd.includes('https://en.wikipedia.org') ? toadd : 'https://en.wikipedia.org' + toadd;
+                                    todo[level + 1].push([urlToAdd, anchor]);
+                                }
+                            }
+                        }
+                    });
+                    var anchor = combo[1];
+                    if (containsKeyword(url, anchor)) {
+                        name = name.replace(/[\\*?:"<>|]/g, function (x) {
+                            return x.charCodeAt(0);
+                        });
+                        console.log(name);
+                        filename = './focusedDownloads/' + name.toLowerCase() + '.txt';
+                        fs.writeFileSync(filename, body);
+                        URLs.push(thisUrl);
+                    }
+
+                    afterFunction();
+
+                } else {
+                    afterFunction();
+                }
+            }
+        });
+
+    } else {
+        if (level < maxLevel && URLs.length < maxCrawlPageNum) {
+            console.log(URLs.length);
+            console.log(visited.size);
+            console.log(todo[level].length);
+            console.log(level);
+            crawl();
+        } else {
+            outputURLs();
+        }
+    }
+}
 
 function containsKeyword(url, anchor) {
     if (url === undefined)
@@ -32,85 +115,24 @@ function containsKeyword(url, anchor) {
     if (anchor !== undefined)
         anchor = anchor.toLowerCase();
     return (url.indexOf(keyword) !== -1
-                && (url.charAt(url.indexOf(keyword) - 1) === '_'
-                    || url.charAt(url.indexOf(keyword) - 1) === '/'))
-            || (anchor !== undefined
-                && anchor.indexOf(keyword) !== -1
-                    && (anchor.indexOf(keyword) === 0
-                        || anchor.charAt(anchor.indexOf(keyword) - 1) === ' '));
+        && (url.charAt(url.indexOf(keyword) - 1) === '_'
+        || url.charAt(url.indexOf(keyword) - 1) === '/'))
+        || (anchor !== undefined
+        && anchor.indexOf(keyword) !== -1
+        && (anchor.indexOf(keyword) === 0
+        || anchor.charAt(anchor.indexOf(keyword) - 1) === ' '));
 }
 
-function crawl() {
-    counter = counter + 1;
-    var combo = undefined;
-    if (ptodo.length !== 0)
-        combo = ptodo.shift();
-    else
-        combo = todo.shift();
-
-    console.log(combo);
-    var url = combo[0];
-    var level = combo[2];
-    var nextLevel = level + 1;
-    //download the html
-    var name = url.split('/');
-    name = name[name.length - 1];
-
-    request(url, function (err, response, body) {
-        if (err) {
-            console.log(err);
-        } else if (response && response.statusCode === 200 && body) {
-            var $ = cheerio.load(body);
-            var content = '.mw-parser-output';
-
-            var flag = containsKeyword(url, combo[1]);
-            $('a', content).each(function (i, el) {
-                if (!$(this).attr("class:contains('image')")
-                    && $(this).attr('href')) {
-                    var toadd = $(this).attr('href');
-                    var anchor = $(this).attr('title');
-                    if (toadd.includes('/wiki/')
-                        && !toadd.includes(':')
-                        && !toadd.includes('/File')
-                        && !toadd.includes('#')) {
-
-                        var lowerURL = toadd.toLowerCase();
-                        if (nextLevel <= maxLevel && !visited.has(lowerURL)) {
-                            visited.add(lowerURL);
-                            var urlToAdd = toadd.includes('https://en.wikipedia.org') ? toadd : 'https://en.wikipedia.org' + toadd;
-                            if (containsKeyword(toadd, anchor)) {
-                                ptodo.unshift([urlToAdd, anchor, nextLevel]);
-                            } else if (flag) {
-                                ptodo.push([urlToAdd, anchor, nextLevel]);
-                            } else {
-                                todo.push([urlToAdd, anchor, nextLevel]);
-                            }
-                        }
-                    }
-                }
-            });
-            var anchor = combo[1];
-            if (containsKeyword(url, anchor)) {
-                //language=JSRegexp
-                name = name.replace(/[\\*?:"<>|]/g, function (x) {
-                    return x.charCodeAt(0);
-                });
-                console.log(name);
-                filename = './focusedDownloads/' + name.toLowerCase() + '.txt';
-                fs.writeFileSync(filename, body);
-                URLs.push(url);
-            }
-        }
-
-        if ((todo.length > 0 || ptodo.length > 0) && URLs.length < maxCrawlPageNum) {
-            console.log(URLs.length);
-            console.log(ptodo.length);
-            console.log(todo.length);
-            setTimeout(crawl, pauseTime);
-        } else {
-            outputURLs();
-        }
-    });
+function afterFunction() {
+    if (level < maxLevel && URLs.length < maxCrawlPageNum) {
+        console.log(URLs.length);
+        console.log(visited.size);
+        console.log(todo[level].length);
+        console.log(level);
+        setTimeout(crawl, pauseTime);
+    } else {
+        outputURLs();
+    }
 }
 
 function outputURLs() {
